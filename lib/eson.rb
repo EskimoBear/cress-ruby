554a18e3616b1c415d58deb23ce1197119fe92b1
benforeva <andre@mobilitti.com>
2015-01-02 06:51:13 -0400

    Wrote parser specification
    Wrote a module comment for Eson::Parser containing a specification in ebnf for the eson grammar.

--- lib/eson.rb
+++ lib/eson.rb
@@ -5,17 +5,30 @@ module Eson
 
   include Vert
   extend self
-
+  
   def read(eson_string)
     validate_json(eson_string)
-    eson_json = Oj.load(eson_string)
-    Eson::Parser.generate_ast(eson_json)
+    json_tokens = Oj.load(eson_string)
+    Eson::Parser.generate_ast(json_tokens)
   end
 
+  #The following EBNF rules describe the eson grammar. All terminal
+  #symbols that begin with 'JSON_' reference those terminal symbols
+  #defined in the JSON grammar.
+  #
+  #doc = '{',statement, {statement}, '}', EOF;
+  #statement = line, NEWLINE;
+  #line = weak-single | record;
+  #special-forms = "ref" | "def" | "doc";
+  #symbol = [special-forms];
+  #weak-single = "&", symbol, :, JSON_array | JSON_null | weak-single;
+  #single = '{' , weak-single, '}';
+  #record = JSON_pair;  
   module Parser
 
     extend self
-    def generate_ast(eson_json)
+  
+    def generate_ast(json_tokens)
     end
   end
 end

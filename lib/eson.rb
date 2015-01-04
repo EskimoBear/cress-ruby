17ee5821aeecea48695201fca8bada9f044d640b
benforeva <andre@mobilitti.com>
2015-01-04 07:46:19 -0400

    Moved specification to tokenize_program method
    Added `Tokenizer` specification as a yard custom tag @eskimobear.specification to documentation for `#tokenize_program`.

--- lib/eson.rb
+++ lib/eson.rb
@@ -8,54 +8,61 @@ module Eson
   
   def read(eson_program)
     validate_json(eson_program)
-    Eson::Tokenizer.emit_tokens(eson_program)
+    Tokenizer.tokenize_program(eson_program)
   end
 
-  #Specification - Convert an eson program into a sequence of tokens.
-  #
-  # Eson token set, ET is a set of the eson terminal symbols defined below
-  # ---EBNF
-  # program_start = "{";
-  # program_end = "}";
-  # end_of_file = EOF;
-  # array_start = "[";
-  # array_end = "]";
-  # comma = ",";
-  # colon = ":";
-  # proc_prefix = "&";
-  # variable_prefix = "$";
-  # string = {char};
-  # char = JSON_char;
-  # whitespace = " ";
-  # number = JSON_number;
-  # true = JSON_true;
-  # false = JSON_false;
-  # null = JSON_null;
-  # let = "let";
-  # ref = "ref";
-  # doc = "doc";
-  # ---EBNF
-  # Eson token, et is a member of ET
-  # Input program, P - assume a non empty and valid JSON string
-  # Token sequence, T
-  #
-  # Init : length(P) > 0
-  #        length(T) = 0
-  # Next : length(P') = length(P) - value_length(et)
-  #        length(T') = length(T) + 1
-  #
-  # Append program_start token onto T
-  # Convert P to a sequence of JSON pairs, 2-tuple arrays of keys and values
-  # Replace each pair with a 3-tuple including the colon token as the middle element
-  # Inspect each key and value in the pair and split into tokens present in ET
-  #   Label each token with it's terminal symbol
-  #   Replace the key or value with an array of these tokens
-  # Append the first 3-tuple to T and then append a comma token
-  # Repeat until the last 3-tuple is reached and then append the last 3-tuple to T
-  # Append program_end token to T 
-  # Flatten T and output
   module Tokenizer
 
+    extend self
+    
+    #Converts an eson program into a sequence of eson tokens
+    #@param eson_program [String] the eson program string provided to Eson#read
+    #@return [Array<Array>] Returns a sequence of the token sequence and the input sequence
+    #@eskimobear.specification
+    # Eson token set, ET is a set of the eson terminal symbols defined below
+    # ---EBNF
+    # program_start = "{";
+    # program_end = "}";
+    # end_of_file = EOF;
+    # array_start = "[";
+    # array_end = "]";
+    # comma = ",";
+    # colon = ":";
+    # proc_prefix = "&";
+    # variable_prefix = "$";
+    # string = {char};
+    # char = JSON_char;
+    # whitespace = " ";
+    # number = JSON_number;
+    # true = JSON_true;
+    # false = JSON_false;
+    # null = JSON_null;
+    # let = "let";
+    # ref = "ref";
+    # doc = "doc";
+    # ---EBNF
+    # Eson token, et is a member of ET
+    # Input program, p, a valid JSON string 
+    # Input sequence, P, a sequence of characters in p
+    # Token sequence, T
+    #
+    # Init : length(P) > 0
+    #        length(T) = 0
+    # Next : length(P') = length(P) - value_length(et)
+    #        length(T') = length(T) + 1
+    #
+    # Append program_start token onto T
+    # Convert P to a sequence of JSON pairs, 2-tuple arrays of keys and values
+    # Replace each pair with a 3-tuple including the colon token as the middle element
+    # Inspect each key and value in the pair and split into tokens present in ET
+    #   Label each token with it's terminal symbol
+    #   Replace the key or value with an array of these tokens
+    # Append the first 3-tuple to T and then append a comma token
+    # Repeat until the last 3-tuple is reached and then append the last 3-tuple to T
+    # Append program_end token to T 
+    # Flatten T and output
+    def tokenize_program(eson_program)
+    end
   end
 
   #Specification - Parse tokens into a abstract syntax tree
@@ -64,8 +71,8 @@ module Eson
   #---EBNF
   #program = program_start, declaration, program_end, [end_of_file];
   #
-  #program_start = '{';
-  #program_end = '}';
+  #program_start = "{";
+  #program_end = "}";
   #end_of_file = EOF;
   #
   #declaration = pair, {comma, pair};
@@ -94,7 +101,7 @@ module Eson
   #
   #(*a variable is a string that can be dereferenced to a value held 
   #  in the value store*)
-  #variable = [whitespace], variable_prefix, {char}, whitespace; 
+  #variable = variable_prefix, {char}, whitespace; 
   #whitespace = " ";
   #variable_prefix = "$";
   #
@@ -112,6 +119,7 @@ module Eson
   #single = program_start, call, program_end;
   #
   #prefix = proc_prefix | variable_prefix;
+  #
   #(*a document is a program that is equivalent to the it's internal value store*)
   #document = program_start, {attribute, [comma, attribute]}, program_end, end_of_file;
   #---EBNF

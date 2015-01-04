8ae75ff311b519aa1264d5fd48053842a663d90b
benforeva <andre@mobilitti.com>
2015-01-03 23:38:37 -0400

    Wrote specification for Tokenizer
    Defined tokens/terminal symbols for the eson grammar and wrote algorithm for breaking up a program into a sequence of tokens.

--- lib/eson.rb
+++ lib/eson.rb
@@ -6,59 +6,120 @@ module Eson
   include Vert
   extend self
   
-  def read(eson_string)
-    validate_json(eson_string)
-    Eson::Parser.generate_ast(eson_string)
+  def read(eson_program)
+    validate_json(eson_program)
+    Eson::Tokenizer.emit_tokens(eson_program)
   end
 
-  #The following EBNF rules describe the eson grammar. All terminal
-  #symbols that begin with 'JSON_' reference those terminal symbols
-  #defined in the JSON grammar.
+  #Specification - Convert an eson program into a sequence of tokens.
   #
-  #program = '{', {declaration}, '}' | single | document, EOF;
+  #The following is a list of terminal symbols/tokens derived from eson's EBNF grammar rules:
+  #---EBNF
+  #program_start = "{";
+  #program_end = "}";
+  #end_of_file = EOF;
+  #array_start = "[";
+  #array_end = "]";
+  #comma = ",";
+  #colon = ":";
+  #proc_prefix = "&";
+  #variable_prefix = "$";
+  #string = {char};
+  #char = JSON_char;
+  #whitespace = " ";
+  #number = JSON_number;
+  #true = JSON_true;
+  #false = JSON_false;
+  #null = JSON_null;
+  #let = "let";
+  #ref = "ref";
+  #doc = "doc";
+  #---EBNF
   #
-  #declaration = eson_pair, ',';
-  #eson_pair = call | let | attribute | eson_pair_json;
+  #Initialize an array of the eson terminal symbols ET 
+  #Initialize an empty array T and append program_start unto T
+  #Convert the eson program to a sequence of JSON pairs, 2-tuple arrays of keys and values, H
+  #Replace each pair with a 3-tuple with the colon token as the middle element
+  #Inspect each key and value in the pair and split into tokens present in ET
+  #  Label each token with it's terminal symbol
+  #  Replace the key or value with an array of these tokens
+  #Append the first 3-tuple to T and then append a comma token
+  #Repeat until the last 3-tuple is reached and then append the last 3-tuple to T
+  #Append program_end token to T 
+  #Flatten T and output
+  module Tokenizer
+
+  end
+
+  #Specification - Parse tokens into a abstract syntax tree
+  #
+  #The following EBNF rules describe the eson grammar. 
+  #---EBNF
+  #program = program_start, declaration, program_end, [end_of_file];
+  #
+  #program_start = '{';
+  #program_end = '}';
+  #end_of_file = EOF;
+  #
+  #declaration = pair, {comma, pair};
+  #pair = call | let_call | attribute;
+  #comma = ",";
   #
   #(*a call is a declaration performing procedure application without
   #  direct substitution*)
-  #call = procedure, :, JSON_array | JSON_null | single;
-  #procedure = "&", special-form; 
-  #special-form = "let" | "ref" | "doc";
+  #call = procedure, colon, array | null;
   #
-  #(*a single is a type of JSON object allowing
-  # procedure application and substitution*)
-  #single = '{' , call, '}';
+  #procedure = proc_prefix, special-form; 
+  #proc_prefix = "&";
+  #special-form = let | ref | doc;
+  #let = "let";
+  #ref = "ref";
+  #doc = "doc";
+  #colon = ":";
+  #
+  #array = array_start, {element}, array_end;
+  #
+  #array_start = '[';
+  #array_end = ']';
+  #element = value, {comma, value};
+  #value = variable | program | single | document | string | number |
+  #        array | true | false | null;
+  #
+  #(*a variable is a string that can be dereferenced to a value held 
+  #  in the value store*)
+  #variable = [whitespace], variable_prefix, {char}, whitespace; 
+  #whitespace = " ";
+  #variable_prefix = "$";
   #
   #(*the let call performs variable creation *)
-  #let = "&", "let", :, '[', {JSON_string, ','}, ']';
+  #let_call = proc_prefix, let, colon, array_start, let_input,
+  #           {comma, let_input}, array_end;
+  #let_input = string | variable;
   #
-  #(*an attribute is a declaration that evaluates to a JSON_pair performing
-  # simultaneous variable and value creation*)
-  #attribute = JSON_string, :, eson_value;
-  #eson_value = single | identifier, [JSON_string] | JSON_value;
+  #(*an attribute performs simultaneous variable and
+  # value creation*)
+  #attribute = string, colon , value;
   #
-  #(*an identifier is a JSON string that can be dereferenced to a value held 
-  #  in the value store*)
-  #identifier = "$", JSON_string;
+  #(*a single is a program allowing
+  # procedure application and substitution*)
+  #single = program_start, call, program_end;
   #
-  #(*eson_pair_json describes the eson_pair in terms of it's JSON counterpart*)
-  #eson_pair_json = eson_string, :, eson_value;
-  #eson_string = prefix, JSON_string | procedure | identifier, [JSON_string] ;
-  #prefix = ["&", "$"];
-  #
-  #(*a document is the equivalent to the program's internal value store*)
-  #document = '{', {attribute}, '}' | JSON_object, EOF;
-  #
-  #JSON_object = '{', {statement}, '}';
-  #statement = JSON_pair, ',';
-  #JSON_pair = JSON_value, :, JSON_string;
+  #prefix = proc_prefix | variable_prefix;
+  #(*a document is a program that is equivalent to the it's internal value store*)
+  #document = program_start, {attribute, [comma, attribute]}, program_end, end_of_file;
+  #---EBNF
   module Parser
 
     extend self
-  
-    def generate_ast(doc)
-      declarations = get_declarations(doc)
+
+    #Append declaration type to make syntax tuple [declaration, type]
+    #
+    #Returns an array of syntax structs [[declaration, type]]
+    #@param program [String] the eson program
+    #@return [Array] Each syntax tuple is an array pair of the
+    #                form [declarations, type]
+    def generate_ast(program)
+      declarations = get_declarations(program)
     end
 
     #Returns an array of declarations. 

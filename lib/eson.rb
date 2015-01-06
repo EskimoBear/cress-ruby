e7d07d38591e74598bb9eddecd6c031ab763bee4
benforeva <andre@mobilitti.com>
2015-01-06 08:51:54 -0400

    Coversion from JSON symbols to eson tokens completed
    Tokenizer takes a JSON hash and converts it to a sequence of JsonSymbols. Each JsonSymbol is inspected and converted to a sequence of Tokens. JsonSymbols and Tokens are represented as structs, so checking their type is a field lookup on the struct.
    - Uses regexp to break up JSON_value strings into word, other_chars, whitespace and proc_prefix. This will allow for variable dereferencing in value strings.
    - Uses recursion to ensure all nested objects are converted to a JsonSymbol sequence rather than a symbol of type JSON_value.

--- lib/eson.rb
+++ lib/eson.rb
@@ -15,6 +15,9 @@ module Eson
   module Tokenizer
 
     extend self
+
+    JsonSymbol = Struct.new "JsonSymbol", :value, :name
+    Token = Struct.new "Token", :value, :name
     
     #Converts an eson program into a sequence of eson tokens
     #@param eson_program [String] string provided to Eson#read
@@ -25,14 +28,13 @@ module Eson
     # program_start = "{";
     # program_end = "}";
     # end_of_file = EOF;
-    # array_start = "[";
-    # array_end = "]";
     # comma = ",";
     # colon = ":";
     # proc_prefix = "&";
     # variable_prefix = "$";
-    # string = {char};
-    # char = JSON_char;
+    # key_word = {JSON_char};
+    # word = {JSON_char}; 
+    # other_chars = {JSON_char};
     # whitespace = " ";
     # number = JSON_number;
     # true = JSON_true;
@@ -64,104 +66,170 @@ module Eson
     def tokenize_program(eson_program)
       program_sequence = string_to_char_sequence(eson_program)
       token_sequence = Array.new
-      json_p = to_json_symbols(eson_program)
-      tokenize_json_symbols(json_p)
-    end
-    
-    def to_json_symbols(eson_program)
       json_hash = Oj.load(eson_program)
-      json_symbol = Struct.new "JSONsymbol", :value, :name
-      seq = Array.new << json_symbol.new("{","object_start")
-      seq << members_to_json_symbols(json_hash, json_symbol)
-      seq << json_symbol.new("}","object_end")
-      seq.flatten
+      json_symbol_sequence = get_json_symbol_sequence(json_hash)
+      puts tokenize_json_symbols(json_symbol_sequence)
     end
 
-    def members_to_json_symbols(json_pairs, json_symbol)
-      seq = Array.new << pair_to_json_symbols(json_pairs.first, json_symbol)
+    def string_to_char_sequence(string)
+      seq = Array.new
+      string.each_char {|c| seq << c}
+      seq
+    end
+    
+    def get_json_symbol_sequence(hash)
+      Array.new.push(JsonSymbol.new("{","object_start"))
+        .push(members_to_json_symbols(hash))
+        .push(JsonSymbol.new("}","object_end"))
+        .flatten
+    end
+
+    def members_to_json_symbols(json_pairs)
+      seq = Array.new << pair_to_json_symbols(json_pairs.first)
       rest = json_pairs.drop(1)
-      comma = json_symbol.new(",", "comma")
       rest.each_with_object(seq) do |i, seq|
-        seq << comma
-        seq << pair_to_json_symbols(i, json_symbol)
+        seq.push(JsonSymbol.new(",", "comma"))
+          .push(pair_to_json_symbols(i))
       end
     end
 
-    def pair_to_json_symbols(json_pair, json_symbol)
-      colon = json_symbol.new(":", "colon")
-      Array.new << json_symbol.new(json_pair.first, "JSON_key") << colon << json_symbol.new(json_pair[1], "JSON_value")
+    def pair_to_json_symbols(json_pair)
+      value = if json_pair[1].is_a? Hash
+                get_json_symbol_sequence(json_pair[1])
+              else
+                JsonSymbol.new(json_pair[1], "JSON_value")
+              end
+      Array.new.push(JsonSymbol.new(json_pair.first, "JSON_key"))
+        .push(JsonSymbol.new(":", "colon"))
+        .push(value)
+        .flatten
     end
 
     def symbol_length(json_symbol)
       json_symbol.value.length
     end
     
-    def string_to_char_sequence(string)
-      seq = Array.new
-      string.each_char {|c| seq << c}
-      seq
-    end
-
-    def tokenize_json_symbols(sequence)
-      seq = Array.new
-      token = Struct.new "Token", :value, :name
-      sequence.map do |i|
-        case i.name
+    def tokenize_json_symbols(symbol_seq)
+      symbol_seq.each_with_object(Array.new) do |symbol, seq|
+        case symbol.name
         when "object_start"
-          seq << token.new("{","program_start")
+          seq.push(Token.new("{", "program_start"))
         when "object_end"
-          seq << token.new("}","program_end")
+          seq.push(Token.new("}", "program_end"))
         when "colon"
+          seq.push(Token.new(":", "colon"))
         when "comma"
+          seq.push(Token.new(",", "comma"))
         when "JSON_key"
-          tokenize_json_key(i.value, seq)
+          tokenize_json_key(symbol.value, seq)
         when "JSON_value"
-          tokenize_json_value(i.value, seq)
+          tokenize_json_value(symbol.value, seq)
         end
       end
-      puts seq
     end
 
     def tokenize_json_key(json_key, seq)
-      token = Struct.new "Token", :value, :name
-      eson_prefix = get_eson_prefix(json_key)
-      case eson_prefix
-      when "$"
-        seq << token.new(eson_prefix, "variable_prefix")
-        seq << token.new(get_prefixed_string(json_key), "string")
-      when "&"
-        seq << token.new(eson_prefix, "proc_prefix")
-        seq << token.new(get_prefixed_string(json_key), "string")
-      when ""
-        seq << token.new(json_key, "string")
+      if begins_with_proc_prefix?(json_key)
+        seq.push(Token.new("&", "proc_prefix"))
+        tokenize_words_and_special_forms(get_prefixed_string(json_key), seq)
+      else
+        seq.push(Token.new(json_key, "key_word"))
       end
     end
-    
-    def get_eson_prefix(string)
-      string.match(/\A\$|&/).to_s
-    end
 
+    def begins_with_proc_prefix?(string)
+      string[0] == "&"
+    end
+    
     def get_prefixed_string(string)
-       string.match(/\A\$|&/).post_match     
+      string[1..-1]     
     end
 
     def tokenize_json_value(json_value, seq)
-      token = Struct.new "Token", :value, :name
       if json_value.is_a? TrueClass
-        seq << token.new(json_value, "true")
-      elsif json_value.is_a? FalseClass
-        seq << token.new(json_value, "false")
+        seq.push(Token.new(json_value, "true"))
       elsif json_value.is_a? Numeric
-        seq << token.new(json_value, "number")
+        seq.push(Token.new(json_value, "number"))
+      elsif json_value.is_a? Array
+        seq.push(Token.new(json_value, "array"))
+      elsif json_value.is_a? FalseClass
+        seq.push(Token.new(json_value, "false"))
       elsif json_value.nil?
-        seq << token.new(json_value, "null")
+        seq.push(Token.new(json_value, "null"))
       elsif json_value.is_a? String
-        seq << token.new(json_value, "string")
+        tokenize_json_string(json_value, seq)
       end
     end
-    
-  end
 
+    def tokenize_json_string(json_string, seq)
+      if json_string.empty?
+        seq
+      elsif match_leading_whitespace_or_variable_prefix?(json_string)
+        tokenize_prefix(json_string, seq)
+        tokenize_json_string(get_prefixed_string(json_string), seq)
+      elsif match_other_chars?(json_string)
+        other_chars, rest = get_other_chars_and_string(json_string)
+        seq.push(Token.new(other_chars, "other_chars"))
+        tokenize_json_string(rest, seq)
+      else
+        next_word, rest = get_next_word_and_string(json_string)
+        tokenize_words_and_special_forms(next_word, seq)
+        tokenize_json_string(rest, seq)
+      end
+    end
+
+    def match_leading_whitespace_or_variable_prefix?(string)
+      string.match(/\A(\$|\s)/).to_s == "" ? false : true   
+    end
+
+    def tokenize_prefix(json_string, seq)
+      case next_char(json_string)
+     when "$"
+        seq.push(Token.new("$", "variable_prefix"))
+      when " "
+        seq.push(Token.new(" ", "whitespace"))
+      end
+    end
+
+    def match_other_chars?(string)
+      string.match(/\A[\W]*/).to_s == "" ? false : true
+    end
+
+    def get_other_chars_and_string(string)
+      other_chars = string.match(/\A[\W]*/)
+      rest_start_index = other_chars.end(0)
+      [other_chars.to_s, string[rest_start_index..-1]]
+    end
+
+    def next_char(string)
+      string[0]
+    end
+
+    def get_next_word_and_string(string)
+      next_word = string.match(/[a-zA-Z\-_.\d]*/)
+      rest_start_index = next_word.end(0)
+      [next_word.to_s, string[rest_start_index..-1]]
+    end
+    
+    def tokenize_words_and_special_forms(json_string, seq)
+      special_form = match_special_form(json_string)
+      case special_form
+      when "doc"
+        seq.push(Token.new(json_string, "doc"))
+      when "let"
+        seq.push(Token.new(json_string, "let"))
+      when "ref"
+        seq.push(Token.new(json_string, "ref")) 
+      when ""
+        seq.push(Token.new(json_string, "word"))
+      end      
+    end
+
+    def match_special_form(string)
+      string.match(/\A(let|ref|doc)/).to_s
+    end
+  end
+  
   #Specification - Parse tokens into a abstract syntax tree
   #
   #The following EBNF rules describe the eson grammar. 
@@ -178,7 +246,7 @@ module Eson
   #
   #(*a call is a declaration performing procedure application without
   #  direct substitution*)
-  #call = procedure, colon, array | null;
+  #call = procedure, colon, array | null | single;
   #
   #procedure = proc_prefix, special-form; 
   #proc_prefix = "&";
@@ -188,24 +256,25 @@ module Eson
   #doc = "doc";
   #colon = ":";
   #
-  #array = array_start, {element}, array_end;
-  #
-  #array_start = '[';
-  #array_end = ']';
-  #element = value, {comma, value};
-  #value = variable | program | single | document | string | number |
+  #value = variable_identifier | string | single | document | number |
   #        array | true | false | null;
   #
-  #(*a variable is a string that can be dereferenced to a value held 
+  #(*a variable_identifier is a string that can be dereferenced to a value held 
   #  in the value store*)
-  #variable = variable_prefix, word, whitespace;
-  #word = string;
-  #whitespace = " ";
+  #variable_identifier = variable_prefix, word;
   #variable_prefix = "$";
+  #
+  #string = [whitespace | variable_prefix], [word | other_chars],
+  #         {[whitespace | variable_prefix], [word | other_chars]};
+  #whitespace = " ";
+  #word = {char}; (*letters, numbers, '-', '_', '.'*)
+  #other_chars = {char}; (*characters excluding those found
+  #   in variable_prefix, word and whitespace*)
   #
   #(*an attribute performs simultaneous variable and
   # value creation*)
-  #attribute = string, colon, value;
+  #attribute = key_word, colon, value;
+  #key_word = {char} (*all characters excluding proc_prefix*)
   #
   #(*a single is a program allowing
   # procedure application and substitution*)
@@ -213,7 +282,7 @@ module Eson
   #
   #prefix = proc_prefix | variable_prefix;
   #
-  #(*a document is a program that is equivalent to the it's internal value store*)
+  #(*a document is a program that contains only attributes*)
   #document = program_start, {attribute, [comma, attribute]}, program_end, end_of_file;
   #---EBNF
   module Parser

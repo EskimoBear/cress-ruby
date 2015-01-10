8f21cf927a7bbb10f36945d343ffa94ca9a8ec06
benforeva <andre@mobilitti.com>
2015-01-10 12:33:15 -0400

    Include only non-terminals in parser specification
    Moved full eson grammar to eson.rb and left only non-terminals grammar rules in Parser specification.

--- lib/eson/parser.rb
+++ lib/eson/parser.rb
@@ -8,32 +8,22 @@ module Eson
     #
     #Returns an array of syntax structs [[declaration, type]]
     #@param token_sequence [Array] the eson token sequence
-    #@return 
+    #@return [Hash] abstract syntax tree
     #@eskimobear.specification
     #The following EBNF rules describe the eson grammar. 
     #---EBNF
     #program = program_start, declaration, program_end, [end_of_file];
     #
-    #program_start = "{";
-    #program_end = "}";
-    #end_of_file = EOF;
-    #
     #declaration = pair, {comma, pair};
     #pair = call | attribute;
-    #comma = ",";
     #
     #(*a call is a declaration performing procedure application without
     #  direct substitution*)
     #call = procedure, colon, array | null | single;
     #
     #procedure = proc_prefix, special_form; 
-    #proc_prefix = "&";
+    #
     #special_form = let | ref | doc | unknown_special_form;
-    #let = "let";
-    #ref = "ref";
-    #doc = "doc";
-    #unknown_special_form = {char};
-    #colon = ":";
     #
     #value = variable_identifier | string | single | document | number |
     #        array | true | false | null;
@@ -41,14 +31,9 @@ module Eson
     #(*a variable_identifier is a string that can be dereferenced to a value held 
     #  in the value store*)
     #variable_identifier = variable_prefix, word;
-    #variable_prefix = "$";
     #
     #string = [whitespace | variable_prefix], [word | other_chars],
     #         {[whitespace | variable_prefix], [word | other_chars]};
-    #whitespace = " ";
-    #word = {char}; (*letters, numbers, '-', '_', '.'*)
-    #other_chars = {char}; (*characters excluding those found
-    #   in variable_prefix, word and whitespace*)
     #
     #(*an attribute performs simultaneous variable and
     # value creation*)
@@ -58,11 +43,6 @@ module Eson
     #(*a single is a program allowing
     # procedure application and substitution*)
     #single = program_start, call, program_end;
-    #
-    #prefix = proc_prefix | variable_prefix;
-    #
-    #(*a document is a program that contains only attributes*)
-    #document = program_start, {attribute, [comma, attribute]}, program_end, end_of_file;
     #---EBNF
     def generate_ast(token_sequence)
     end

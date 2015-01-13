0fb67a9a56304bad9d3fcb8a298ec49dda7efcc6
benforeva <andre@mobilitti.com>
2015-01-13 13:11:13 -0400

    Cleaned up Language#initial specification
    Listed rules with non-terminals to the top to make it easier to define the production rules as methods Slightly changed the Structs to use more correct EBNF terminology, also replaced =, 'equal' signs with :=, 'defined as' in production rules. I notice some recursive definitions in the production rules but I'm not sure if those are an issue in terms of defining the non-terminals. I'll find out when I write the methods to create the rules.

--- lib/eson/language.rb
+++ lib/eson/language.rb
@@ -3,78 +3,74 @@ module Eson
   module Language
     extend self
 
+    Terminal = Struct.new(:name, :controls)
+    Rule = Struct.new(:name, :sequence)
+    NonTerminal = Struct.new(:name, :rule, :controls)
     #Return the initial formal language of the compiler
-    #@return [Language] the initial eson language as a Struct
+    #@return L0 the initial eson language
     #@eson.specification
+    # Prop : L0 is a struct of eson production rules
+    #      : production rule, a Rule struct of name and sequence
+    #      : name, symbol of Rule, Terminal or NonTerminal
+    #      : sequence, the EBNF control for concatenation
+    #          it is an array of NonTerminals and Terminals
+    #      : Terminals are structs with type and operation
+    #      : NonTerminals are structs with type, rule and operation
+    #      : controls is the set of EBNF controls applied to a
+    #          Terminal or NonTerminal. Consisting of :choice,
+    #          :option and/or :repetition. The sequence control is
+    #          implicit in the ordering of Rule's sequence member.
     #
-    #A Language is a struct of production rules
-    #A Rule or production rule is a struct of type and concatenation
-    #A concatenation is an array of NonTerminals and Terminals
-    #Terminals are structs with type, lexeme and operation
-    #NonTerminals are structs with type, rule_type and operation
-    #operation is an enum of either :alternation, :option or :repetition as
-    # defined by EBNF
-    #the concatenation rule operation is implicit in the ordering of
-    #  concatenation
+    #The following EBNF rules describe the eson grammar, L0:
+    # variable_prefix := "$";
+    # word := {JSON_char}; (*letters, numbers, '-', '_', '.'*)
+    # variable_identifier := variable_prefix, word;
     #
-    #The following EBNF rules describe the eson grammar. 
-    #---EBNF
-    #program = program_start, [declaration], program_end, [end_of_file];
-    #
-    #program_start = "{";
-    #program_end = "}";
-    #end_of_file = EOF;
-    #
-    #declaration = pair, declaration_list;
-    #declaration_list = {comma, pair};
-    #
-    #pair = call | attribute;
-    #comma = ",";
-    #
-    #(*a call is a declaration performing procedure application without
-    #  direct substitution*)
-    #call = procedure, colon, call_value;
-    #call_value = array | null | single;
-    #
-    #procedure = proc_prefix, special_form; 
-    #proc_prefix = "&";
-    #special_form = let | ref | doc | unknown_special_form;
-    #let = "let";
-    #ref = "ref";
-    #doc = "doc";
-    #unknown_special_form = {char};
-    #colon = ":";
-    #
-    #value = variable_identifier | string | single | number |
-    #        array | true | false | null;
-    #boolean = true | false;
-    #
-    #(*a variable_identifier is a string that can be dereferenced to a value held 
-    #  in the value store*)
-    #variable_identifier = variable_prefix, word;
-    #variable_prefix = "$";
-    #
-    #string = [whitespace | variable_prefix], [word | other_chars],
-    #         {[whitespace | variable_prefix], [word | other_chars]};
-    #whitespace = " ";
-    #word = {char}; (*letters, numbers, '-', '_', '.'*)
-    #other_chars = {char}; (*characters excluding those found
+    # whitespace := " ";
+    # other_chars := {JSON_char}; (*characters excluding those found
     #   in variable_prefix, word and whitespace*)
+    # word_forms := whitespace | variable_prefix | word | other_chars;
+    # string := {word_forms};
     #
-    #array = array_start, value, array_list, array_end;
-    #array_list = {comma, value}
+    # true := JSON_true;
+    # false := JSON_false;
+    # boolean = true | false;
     #
-    #(*an attribute performs simultaneous variable and
-    # value creation*)
-    #attribute = key_word, colon, value;
-    #key_word = {char} (*all characters excluding proc_prefix*)
+    # number := JSON_number;
+    # null := JSON_null;
+    # value := variable_identifier | string | number | boolean |
+    #          boolean | null | array | single;
     #
-    #(*a single is a program allowing
-    # procedure application and substitution*)
-    #single = program_start, call, program_end;
+    # array_start := "[";
+    # array_end := "]";
+    # comma := ",";
+    # element_more := {comma, value}
+    # element_list := value, element_more    
+    # array := array_start, [element_list], array_end;
     #
-    #prefix = proc_prefix | variable_prefix;
-    #---EBNF
+    # let := "let";
+    # ref := "ref";
+    # doc := "doc";
+    # unknown_special_form := {JSON_char};
+    # proc_prefix := "&";
+    # special_form := let | ref | doc | unknown_special_form;
+    # procedure := proc_prefix, special_form;
+    #
+    # colon := ":";
+    # call_value := array | null | single;
+    # call := procedure, colon, call_value;
+    #
+    # program_start := "{";
+    # program_end := "}";
+    # single := program_start, call, program_end;
+    #
+    # key_word := {JSON_char}; (*all characters excluding proc_prefix*)
+    # attribute := key_word, colon, value;
+    #
+    # pair := call | attribute;
+    # declaration_more := comma, pair;
+    # declaration_list := pair, {declaration_more};
+    # program := program_start, [declaration_list], program_end;
     def initial
       rules = {program: program_rule,
                declaration: declaration_rule,
@@ -91,6 +87,20 @@ module Eson
                array_list: array_list_rule,
                attribute: attribute_rule,
                single: single_rule}
+      L0 = Struct.new *rules.keys
+      L0.new *rules.values
+    end
+
+    private
+   
+    def boolean_rule
+      Rule[:boolean, Array.new.push(Terminal[:true, :choice])
+                     .push(Terminal[:false, :choice])]
+    end
+    
+    def variable_identifier_rule
+      Rule[:variable_identifier, Array.new.push(Terminal[:variable_prefix])
+                                 .push(Terminal[:word])]
     end
   end
 end

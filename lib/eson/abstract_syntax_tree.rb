6e2762554b7f1935d54c4623ad02f609c5e065d2
benforeva <andre@mobilitti.com>
2015-02-16 16:27:47 -0400

    Added active_node method to AST
    Added method to determine the active_node in the tree. It currently works only for the case of the newly created tree in which the active node is the root node. This also introduces the idea of the open and closed tree node. Both can be checked using the open? and closed? methods respectively. All newly created tree nodes are given an open status of true via a struct field.

--- lib/eson/abstract_syntax_tree.rb
+++ lib/eson/abstract_syntax_tree.rb
@@ -33,6 +33,63 @@ module Eson
     Token = "Eson::Tokenizer::TokenSeq::Token"
     Rule = "Eson::Language::RuleSeq::Rule"
 
+    #Struct class for a tree node
+    Tree = Struct.new :rule, :children, :open_state do
+
+      #The value of the root node
+      #@return [Eson::Language::RuleSeq::Rule]
+      def root_value
+        rule
+      end
+
+      #The open state of the root node. A node is open
+      #if it is possible to add another child node.
+      #@return [Boolean]
+      def open?
+        open_state
+      end
+
+      def closed?
+        !open?
+      end
+
+      def empty?
+        children.empty?
+      end
+           
+      def add_node(node)
+        case node.class.to_s
+        when Rule
+          tree = Eson::AbstractSyntaxTree::Tree.new(node, TreeSeq.new)
+          self.children.push(tree)
+        when Token
+          if valid_token? node
+            self.children.push(node)
+          else
+            raise TreeInsertionError, not_a_valid_token_error_message(node)
+          end
+        else
+          raise TreeInsertionError, not_a_valid_node_type_error_message(node)
+        end
+      end
+
+      def valid_token?(token)
+        self.rule.contains_terminal? token.name
+      end
+
+      def degree
+        self.children.length
+      end
+
+      def not_a_valid_token_error_message(token)
+        "The token #{token.name} does not constitute legal syntax in the available position."
+      end
+
+      def not_a_valid_node_type_error_message(node)
+        "The class #{node.class} of '#{node}' is not a valid node for the #{self.class}. Must be either a #{Token} or a #{Rule}."
+      end
+    end
+    
     class TreeSeq < Array
 
       Tree = "Eson::AbstractSyntaxTree::Tree"
@@ -53,61 +110,42 @@ module Eson
       prepend pushvalidate
 
       def not_a_valid_node_error_message(obj)
-        "The class #{obj.class} of '#{obj}' is not a valid node for the #{self.class}. Must be either a #{Token} or a #{Rule}."
-      end
-    end
-        
-    Tree = Struct.new :rule, :children do
-      
-      def add_node(node)
-        case node.class.to_s
-        when Rule
-          tree = Eson::AbstractSyntaxTree::Tree.new(node, TreeSeq.new)
-          self.children.push(tree)
-        when Token
-          if valid_token? node
-            self.children.push(node)
-          else
-            raise TreeInsertionError, not_a_valid_token_error_message(node)
-          end
-        else #raise error
-          self.children.push(node)
-        end
-      end
-
-      def valid_token?(token)
-        self.rule.contains_terminal? token.name
-      end
-
-      def degree
-        self.children.length
-      end
-
-      def not_a_valid_token_error_message(token)
-        "The token #{token.name} does not constitute legal syntax in any available position."
+        "The class #{obj.class} of '#{obj}' is not a valid node for the #{self.class}. Must be either a #{Token} or a #{Tree}."
       end
     end
 
-    #Initialize tree with the main production rule of a formal language
-    #@param language [Eson::Language::e0] eson formal language
+    #Initialize tree with the main production rule of a
+    #formal language
+    #@param language [Eson::Language::e0] formal language
     def initialize(language)
       if language.respond_to?(:top_rule)
         @language = language
-        @tree = Eson::AbstractSyntaxTree::Tree.new(language.top_rule, TreeSeq.new)
+        @tree = Eson::AbstractSyntaxTree::Tree
+                .new(language.top_rule, TreeSeq.new, true)
+        @active = @tree
       else
-        raise TreeInitializationError, not_a_valid_language_error_message(language)
+        raise TreeInitializationError,
+              not_a_valid_language_error_message(language)
       end
     end
-    
+     
     def not_a_valid_language_error_message(language)
       "'#{language.class}' is not a valid language for #{self.class}."
+    end
+
+    #Get the active node of the tree. This is the open tree node to
+    #the bottom right of the tree. If this tree node is closed, the
+    #active node is it's next open ancestor.  
+    #@return [Eson::AbstractSyntaxTree::Tree] the active tree node
+    def active_node
+      @active
     end
 
     def get
       @tree
     end
 
-    def_delegators :@tree, :rule, :children, :add_node, :degree
+    def_delegators :@tree, :root_value, :closed?, :open?, :empty?, :rule, :children, :add_node, :degree
   end
 end
 
--- test/abstract_syntax_tree_test.rb
+++ test/abstract_syntax_tree_test.rb
@@ -7,28 +7,45 @@ describe Eson::AbstractSyntaxTree do
 
   before do
     @ast = Eson::AbstractSyntaxTree.new(Eson::Language.e5)
+    @token = Eson::Tokenizer::TokenSeq::Token.new(",", :comma)
+    @rule = Eson::Language::e0.variable_identifier
   end
 
-  it "get an empty tree" do
-    @ast.must_be_instance_of Eson::AbstractSyntaxTree
-    @ast.get.rule.must_equal Eson::Language.e5.top_rule
+  describe "create an e5 tree" do
+    before do
+      @tree = Eson::AbstractSyntaxTree.new(Eson::Language.e5)
+    end
+    it "root is top rule" do 
+      @tree.must_be_instance_of Eson::AbstractSyntaxTree
+      @tree.root_value.must_equal Eson::Language.e5.top_rule
+    end
+    it "root is open" do
+      @tree.open?.must_equal true
+      @tree.closed?.must_equal false
+    end
+    it "root is active" do
+      @tree.active_node.must_equal @tree.get 
+      @tree.active_node.must_be_instance_of Eson::AbstractSyntaxTree::Tree
+    end
   end
 
   describe "#add_node" do
-    it "has invalid node type" do
-      proc {@ast.add_node("poo")}.must_raise Eson::AbstractSyntaxTree::TreeSeqInsertionError
+    it "node is invalid type" do
+      proc {@ast.add_node("poo")}.must_raise Eson::AbstractSyntaxTree::TreeInsertionError
     end
-    it "is invalid token" do
+    it "node is a Rule" do
+      @ast.add_node(@rule)
+      @ast.children.wont_be_empty
+    end
+    it "node is a Token" do
+      @ast.add_node(@token)
+      @ast.children.wont_be_empty
+    end
+    it "node is invalid token" do
       skip
-      
       token = Eson::Tokenizer::TokenSeq::Token.new(",", :comma)
       proc {@ast.add_node(token)}.must_raise Eson::AbstractSyntaxTree::TreeInsertionError
       @ast.children.must_be_empty
-    end
-    it "add a rule" do
-      rule = Eson::Language::e0.variable_identifier
-      @ast.add_node(rule)
-      @ast.children.wont_be_empty
     end
   end
 end

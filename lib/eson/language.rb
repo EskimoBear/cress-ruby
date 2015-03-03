32df991bf53496f6085107046ff5aff1e26b909e
benforeva <andre@mobilitti.com>
2015-03-03 18:57:04 -0400

    Refactored ebnf related operations
    Moved all operations that act on the Rule's ebnf field into the EBNF module. All calls to Rule, the module's receiver class are marked by self for clarity.

--- lib/eson/language.rb
+++ lib/eson/language.rb
@@ -25,6 +25,8 @@ module Eson
       end              
     end
 
+    #Operations and data structures for the ebnf field
+    #  of the Eson::Language::RuleSeq::Rule
     module EBNF
 
       Terminal = Struct.new(:rule_name)
@@ -34,7 +36,78 @@ module Eson
       AlternationRule = Struct.new(:term_set)
       RepetitionRule = Struct.new(:term)
       OptionRule = Struct.new(:term)
+
+      def terminal?
+        self.ebnf.nil?
+      end
       
+      def nonterminal?
+        !terminal?
+      end
+
+      def nullable?
+        if self.option_rule? || self.repetition_rule?
+          true
+        else
+          false
+        end
+      end
+
+      def term_names
+        if self.terminal?
+          nil
+        elsif alternation_rule?
+          self.ebnf.term_set.map{|i| i.rule_name}
+        elsif concatenation_rule?
+          self.ebnf.term_list.map{|i| i.rule_name}
+        elsif repetition_rule? || option_rule?
+          [ebnf.term.rule_name]
+        end
+      end
+
+      #FIXME this no longer works as terminals which have been
+      #converted from nonterminals have an undefined @start_rxp
+      def to_s       
+        "#{name} := #{self.ebnf_to_s};"
+      end
+
+      def ebnf_to_s
+        if terminal?
+          "\"#{rxp.source.gsub(/\\/, "")}\""
+        elsif alternation_rule?
+          terms = ebnf.term_set
+          join_rule_names(terms, " | ")
+        elsif concatenation_rule?
+          terms = ebnf.term_list
+          join_rule_names(terms, ", ")
+        elsif repetition_rule?
+          "{#{ebnf.term.rule_name}}"
+        elsif option_rule?
+          "[#{ebnf.term.rule_name}]"
+        end
+      end
+
+      def alternation_rule?
+        self.ebnf.instance_of? AlternationRule
+      end
+
+      def concatenation_rule?
+        self.ebnf.instance_of? ConcatenationRule
+      end
+
+      def repetition_rule?
+        self.ebnf.instance_of? RepetitionRule
+      end
+
+      def option_rule?
+        self.ebnf.instance_of? OptionRule
+      end
+
+      def join_rule_names(terms, infix="")
+        initial = terms.first.rule_name.to_s
+        rest = terms.drop(1)
+        rest.each_with_object(initial){|i, memo| memo.concat(infix).concat(i.rule_name.to_s)}
+      end
     end
     
     class RuleSeq < Array
@@ -77,68 +150,6 @@ module Eson
           self.new(name, start_rxp) 
         end
 
-        def nullable?
-          if option_rule? || repetition_rule?
-            true
-          else
-            false
-          end
-        end
-
-        def term_names
-          if terminal?
-            nil
-          elsif alternation_rule?
-            ebnf.term_set.map{|i| i.rule_name}
-          elsif concatenation_rule?
-            ebnf.term_list.map{|i| i.rule_name}
-          elsif repetition_rule? || option_rule?
-            [ebnf.term.rule_name]
-          end
-        end
-        
-        #FIXME this no longer works as terminals which have been
-        #converted from nonterminals have an undefined @start_rxp
-        def to_s       
-          "#{name} := #{ebnf_to_s};"
-        end
-
-        def ebnf_to_s
-          if terminal?
-            "\"#{rxp.source.gsub(/\\/, "")}\""
-          elsif alternation_rule?
-            terms = ebnf.term_set
-            join_rule_names(terms, " | ")
-          elsif concatenation_rule?
-            terms = ebnf.term_list
-            join_rule_names(terms, ", ")
-          elsif repetition_rule?
-            "{#{ebnf.term.rule_name}}"
-          elsif option_rule?
-            "[#{ebnf.term.rule_name}]"
-          end
-        end
-
-        def alternation_rule?
-          ebnf.instance_of? EBNF::AlternationRule
-        end
-
-        def concatenation_rule?
-          ebnf.instance_of? EBNF::ConcatenationRule
-        end
-
-        def repetition_rule?
-          ebnf.instance_of? EBNF::RepetitionRule
-        end
-
-        def option_rule?
-          ebnf.instance_of? EBNF::OptionRule
-        end
-
-        def join_rule_names(terms, infix="")
-          initial = terms.first.rule_name.to_s
-          rest = terms.drop(1)
-          rest.each_with_object(initial){|i, memo| memo.concat(infix).concat(i.rule_name.to_s)}
         end
         
         def match(string)
@@ -165,14 +176,6 @@ module Eson
           #does not catch zero or more matches that return "", the empty string
           (string =~ apply_at_start(regex)).nil? ? false : true
         end      
-
-        def terminal?
-          self.ebnf.nil?
-        end
-        
-        def nonterminal?
-          !terminal?
-        end
 
         def apply_at_start(regex)
           /\A#{regex.source}/

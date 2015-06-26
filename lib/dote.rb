3589879194407c05902c16d33c88e92bc1dc2b6b
benforeva <andre@mobilitti.com>
2015-06-26 09:18:26 -0400

    Refactored dote.rb
    - Removed references to former name eson
    - Documented .compile method

--- lib/dote.rb
+++ lib/dote.rb
@@ -14,25 +14,29 @@ module Dote
   EMPTY_PROGRAM = "empty_program"
   MALFORMED_PROGRAM = "Program is malformed"
 
-  def compile(eson_program, grammar=LANG)
-    if validate_json?(eson_program)
-      token_sequence = TokenPass.tokenize_program(eson_program, grammar)
+  #@param program [String] string representation of the program
+  #@param grammar [Struct] grammar to process program
+  #@return [nil, Parser::ParseTree] parse tree for the program
+  #@raise SyntaxError, when program is malformed JSON
+  def compile(program, grammar=LANG)
+    if validate_json?(program)
+      token_sequence = TokenPass.tokenize_program(program, grammar)
                        .verify_special_forms
       tree = SyntaxPass.build_tree(token_sequence, grammar)
     else
-      validation_pass(eson_program)
+      validation_pass(program)
     end
   end
 
-  def validation_pass(eson_program)
+  def validation_pass(program)
     options = {:custom_errors =>
                {:empty_json_object => EMPTY_PROGRAM,
                 :malformed_json => MALFORMED_PROGRAM}}
-    case validate_json(eson_program, options)
+    case validate_json(program, options)
     when EMPTY_PROGRAM
       return nil
     when Regexp.new(MALFORMED_PROGRAM)
-      raise SyntaxError, validate_json(eson_program, options)
+      raise SyntaxError, validate_json(program, options)
     end
   end
 
